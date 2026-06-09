f42b0a0f456f9f827807ee47453da106cd82974a
Yasir Ahmed Nahid <150611144+yasirahmednahid@users.noreply.github.com>
2026-06-09 16:17:32 +0600

    Add reverse complement functionality for DNA strings

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,10 @@
-
+from codeop import compile_command
+pattern = input("Enter DNA String:")
+compilement = {"A":"T",
+               "T":"A",
+               "C":"G",
+               "G":"C"
+               }
+               reverse_complement = ""
+               for i in range(len(pattern) -1, -1, -1):
+                reverse_complement += complement
